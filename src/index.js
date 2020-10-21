da25a7e1cd41b53e5412fdd43ce836e5327f4c46
Szymon Kabelis <szkabelis@gmail.com>
2020-10-21 20:50:04 +0200

    part-2-loading-assets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import Phaser from "phaser";
 import { CONFIG } from "./constants/config";
+import { SceneLoad } from "./scenes/SceneLoad";
 
 export const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: CONFIG.gameWidth,
   height: CONFIG.gameHeight,
-  scene: [],
+  scene: [SceneLoad],
 };
 
 const game = new Phaser.Game(config);
